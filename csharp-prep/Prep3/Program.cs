79139ddf909d992204c898ba258f77d906c86a11
TristanTA <all20052@byui.edu>
2025-09-22 10:14:42 -0600

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,32 @@ using System;
 
 class Program
 {
+    Random randomGenerator = new Random();
+    int magicNumber;
+    string guessNumber;
+    int intGuessNumber;
+    public Program()
+    {
+        magicNumber = randomGenerator.Next(0, 100);
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        var obj = new Program();
+        do
+        {
+            Console.WriteLine("What is your guess? ");
+            obj.guessNumber = Console.ReadLine();
+            obj.intGuessNumber = int.Parse(obj.guessNumber);
+            if (obj.intGuessNumber > obj.magicNumber)
+            {
+                Console.WriteLine("Your guess is too high... ");
+            }
+            else if (obj.intGuessNumber < obj.magicNumber)
+            {
+                Console.WriteLine("Your guess is too low...");
+            }
+        } while (obj.intGuessNumber != obj.magicNumber);
+
+        Console.WriteLine("You guessed the number!");
     }
 }
