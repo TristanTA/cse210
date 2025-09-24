24c2a89e6816cb963a9d2c407dd7ae4721d5d6c6
TristanTA <all20052@byui.edu>
2025-09-24 09:21:10 -0600

    prep 4 complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        double entered_number;
+        List<double> numbers = new List<double>();
+        do
+        {
+            Console.WriteLine("Please enter a number: ");
+            entered_number = double.Parse(Console.ReadLine());
+            numbers.Add(entered_number);
+        } while (entered_number != 0);
+        double sum = numbers.Sum();
+        double average = numbers.Average();
+        double max = numbers.Max();
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {average}");
+        Console.WriteLine($"Max: {max}");
     }
 }
